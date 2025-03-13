24f7cf51931599f9f70a83c3b49fb65d909c61a5
Rozengerj <108734195+Rozengerj@users.noreply.github.com>
2025-03-13 12:34:05 +0200

    add season and episode js functions nearly finished

---
+++ joro.too.Web/wwwroot/js/AddShowScript.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    //function that acts upon 
+    let seasonsCount = 0;
+
+    $(".widget button").button();
+    $("#AddSeasonBtn").on("click", function (event) {
+        let epcount = 0;
+        $("#SeasonList").append(
+            '<li class="season"> ' +
+            '   <div class="form-container helper" id="s' + seasonsCount + '">' +
+            '      <div class="input-group">' +
+            '        <label class="search-label">'+
+            '         <input type="text" name="season" id="' + seasonsCount + '" class="input" required="" placeholder="Input Season Name Here">' +
+            '         </label>'+          
+            '         <div>' +
+            '            <div class="AddEpisodeDiv">' +
+            '               <button class="AddEpisodeBtn'+seasonsCount+'">Add Episode</button>' +
+            '            </div>'+
+            '         </div>' +
+            '      </div>' +
+            '   <button class="RemoveSeasonBtn">Remove Season</button> ' +
+            '   </div>'+
+            '</li>');
+        $('.RemoveSeasonBtn').click(function (event) {
+            console.log("does this run multiple times??")
+            $(this).closest('.season').remove();
+        });
+        console.log("COUNT UP")
+        let addepstring=".AddEpisodeBtn"+seasonsCount
+        seasonsCount++;
+        console.log(addepstring)
+        $(addepstring).click(function (event) {
+            
+            //$(this).closest('.input').remove();
+            let seasonnum = $(this).closest('.helper').map(function(){
+                return this.id;
+            }).get().join();
+            console.log("are you gonna write shit in here four times i na row you fucking piece of shit")
+            $(this).closest('.AddEpisodeDiv').append(
+                '      <div class="input-group">' +
+                '         <input type="text" name="episode" id="'+ epcount + seasonnum + '" class="input" required="" placeholder="Input Episode Name Here">' +
+                '         <input type="file" name="episodevidsrc" id="'+ epcount + seasonnum + '">' +
+                '      <div>'
+            );
+            epcount++;
+            }
+            //document.getElementById()
+        );
+    });
+});
+
